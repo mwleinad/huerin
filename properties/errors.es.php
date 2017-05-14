986180c288e2968cb892eb7f23136d75e1a71c33
Daniel Lopez <dlopez@trazzos.com>
2017-05-14 09:23:58 -0500

    commit 5

---
+++ properties/errors.es.php
@@ -0,0 +1,314 @@
+<?php
+$property["error"][1] = "Has editado satisfactoriamente un Registro";
+$property["error"][2] = "Has insertado satisfactoriamente un Registro";
+$property["error"][3] = "Has eliminado satisfactoriamente un Registro";
+
+$property["error"][10000] = "La cadena tiene muy pocos caracteres Campo";
+$property["error"][10001] = "No es un URL Valido";
+$property["error"][10002] = "No es un EMail Valido";
+$property["error"][10003] = "El Archivo no existe";
+$property["error"][10004] = "Los passwords no coinciden";
+$property["error"][10005] = "El Email ya esta tomado";
+$property["error"][10006] = "Tu usuario o contrase&ntilde;a es incorrecta";
+$property["error"][10007] = "No has ingresado al sistema.";
+$property["error"][10010] = "No estas autorizado a acceder a esta pagina";
+$property["error"][10011] = "No es un formato de fecha valido";
+$property["error"][10012] = "Este usuario ya esta tomado";
+$property["error"][10013] = "Campo requerido";
+$property["error"][10014] = "Falta ingresar la fecha en uno de los documentos b&aacute;sicos";
+$property["error"][10015] = "Falta ingresar la fecha en uno de las obligaciones o condiciones del contrato";
+$property["error"][10016] = "Ingrese la fecha de la firma";
+$property["error"][10017] = "Falta ingresar la fecha en uno de los documentos sellados";
+$property["error"][10018] = "Ocurri&oacute; un error al enviar el correo";
+$property["error"][10019] = "Por favor, escriba el mensaje";
+
+$property["error"][10020] = "El tipo de contrato fue creado correctamente.";
+$property["error"][10021] = "El tipo de contrato fue actualizado correctamente.";
+$property["error"][10022] = "El tipo de contrato fue eliminado correctamente.";
+
+$property["error"][10023] = "La subcategoria fue creada correctamente.";
+$property["error"][10024] = "La subcategoria fue actualizada correctamente.";
+$property["error"][10025] = "La subcategoria fue eliminada correctamente.";
+
+$property["error"][10026] = "El documento fue creado correctamente.";
+$property["error"][10027] = "El documento fue actualizado correctamente.";
+$property["error"][10028] = "El documento fue eliminado correctamente.";
+
+$property["error"][10029] = "El contrato fue creado correctamente.";
+$property["error"][10030] = "El contrato fue actualizado correctamente.";
+$property["error"][10031] = "El contrato fue eliminado correctamente.";
+
+$property["error"][10032] = "El estado fue creado correctamente.";
+$property["error"][10033] = "El estado fue actualizado correctamente.";
+$property["error"][10034] = "El estado fue eliminado correctamente.";
+
+$property["error"][10035] = "El municipio fue creado correctamente.";
+$property["error"][10036] = "El municipio fue actualizado correctamente.";
+$property["error"][10037] = "El municipio fue eliminado correctamente.";
+
+$property["error"][10038] = "La fecha fue agregada correctamente.";
+$property["error"][10039] = "La fecha fue eliminada correctamente.";
+
+$property["error"][10040] = "La documentaci&oacute;n fue enviada correctamente.";
+$property["error"][10041] = "Por favor, ingrese el correo electr&oacute;nico.";
+$property["error"][10042] = "Por favor, ingrese un correo electr&oacute;nico v&aacute;lido.";
+
+$property["error"][10043] = "El documento fue agregado correctamente.";
+$property["error"][10044] = "El documento fue eliminado correctamente.";
+
+$property["error"][10045] = "El proyecto fue creado correctamente.";
+$property["error"][10046] = "El proyecto fue actualizado correctamente.";
+$property["error"][10047] = "El proyecto fue eliminado correctamente.";
+
+$property["error"][10048] = "El contador fue creado correctamente.";
+$property["error"][10049] = "El contador fue actualizado correctamente.";
+$property["error"][10050] = "El contador fue eliminado correctamente.";
+
+$property["error"][10051] = "El usuario fue creado correctamente.";
+$property["error"][10052] = "El usuario fue actualizado correctamente.";
+$property["error"][10053] = "El usuario fue eliminado correctamente.";
+
+$property["error"][10054] = "La fecha elegida no debe ser mayor a la fecha actual.";
+$property["error"][10055] = "Por favor, ingrese una cantidad v&aacute;lida";
+$property["error"][10056] = "La fecha elegida no debe ser menor a la fecha actual.";
+
+$property["error"][20011] = "Has creado nuevos Folios";
+$property["error"][20012] = "Has borrado satisfactoriamente estos Folios";
+$property["error"][20013] = "Has modificado satisfactoriamente estos Folios";
+
+$property["error"][20023] = "El email ha sido enviado al cliente correctamente";
+$property["error"][20024] = "La factura ha sido cancelada correctamente";
+$property["error"][20025] = "El comprobante ha sido generada exitosamente, ahora puedes proceder a consultar los archivos";
+
+$property["error"][20026] = "Los folios ingresados ya se encuentran agregados, por favor verifique.";
+
+$property["error"][20027] = "Has cancelado el comprobante.";
+
+$property["error"][20039] = "El RFC es requerido si quieres continuar";
+$property["error"][20040] = "Necesitas agregar al menos un concepto en tu comprobante";
+$property["error"][20041] = "Necesitas determinar la Forma de Pago";
+
+$property["error"][20046] = "Debe elegir un tipo de usuario";
+
+$property["error"][20047] = "La serie no existe, o bien has alcanzado el maximo numero de FOLIOS";
+$property["error"][20048] = "No has subido tu Codigo de Barras Bidimensional.";
+
+$property["error"][21042] = "El RFC ya existe";
+$property["error"][21043] = "El aviso fue eliminado correctamente";
+
+//
+$property["error"][23000] = "Se actualiz&oacute; correctamente la nota de cr&eacute;dito";
+$property["error"][23001] = "Se agreg&oacute; correctamente la nota de cr&eacute;dito";
+
+//
+$property["error"][24000] = "Se Agrego correctamente la orden de acabado al almacen PT";
+
+$property["error"][25000] = "Se Agrego correctamente el aviso";
+
+
+$months = array(
+						"01" => "Ene",
+						"02" => "Feb",
+						"03" => "Mar",
+						"04" => "Abr",
+						"05" => "May",
+						"06" => "Jun",
+						"07" => "Jul",
+						"08" => "Ago",
+						"09" => "Sep",
+						"10" => "Oct",
+						"11" => "Nov",
+						"12" => "Dic"
+					);
+					
+$mime_types = array("323" => "text/h323",
+"acx" => "application/internet-property-stream",
+"ai" => "application/postscript",
+"aif" => "audio/x-aiff",
+"aifc" => "audio/x-aiff",
+"aiff" => "audio/x-aiff",
+"asf" => "video/x-ms-asf",
+"asr" => "video/x-ms-asf",
+"asx" => "video/x-ms-asf",
+"au" => "audio/basic",
+"avi" => "video/x-msvideo",
+"axs" => "application/olescript",
+"bas" => "text/plain",
+"bcpio" => "application/x-bcpio",
+"bin" => "application/octet-stream",
+"bmp" => "image/bmp",
+"c" => "text/plain",
+"cat" => "application/vnd.ms-pkiseccat",
+"cdf" => "application/x-cdf",
+"cer" => "application/x-x509-ca-cert",
+"class" => "application/octet-stream",
+"clp" => "application/x-msclip",
+"cmx" => "image/x-cmx",
+"cod" => "image/cis-cod",
+"cpio" => "application/x-cpio",
+"crd" => "application/x-mscardfile",
+"crl" => "application/pkix-crl",
+"crt" => "application/x-x509-ca-cert",
+"csh" => "application/x-csh",
+"css" => "text/css",
+"dcr" => "application/x-director",
+"der" => "application/x-x509-ca-cert",
+"dir" => "application/x-director",
+"dll" => "application/x-msdownload",
+"dms" => "application/octet-stream",
+"doc" => "application/msword",
+"docx" => "application/msword",
+"dot" => "application/msword",
+"dvi" => "application/x-dvi",
+"dxr" => "application/x-director",
+"eps" => "application/postscript",
+"etx" => "text/x-setext",
+"evy" => "application/envoy",
+"exe" => "application/octet-stream",
+"fif" => "application/fractals",
+"flr" => "x-world/x-vrml",
+"gif" => "image/gif",
+"gtar" => "application/x-gtar",
+"gz" => "application/x-gzip",
+"h" => "text/plain",
+"hdf" => "application/x-hdf",
+"hlp" => "application/winhlp",
+"hqx" => "application/mac-binhex40",
+"hta" => "application/hta",
+"htc" => "text/x-component",
+"htm" => "text/html",
+"html" => "text/html",
+"htt" => "text/webviewhtml",
+"ico" => "image/x-icon",
+"ief" => "image/ief",
+"iii" => "application/x-iphone",
+"ins" => "application/x-internet-signup",
+"isp" => "application/x-internet-signup",
+"jfif" => "image/pipeg",
+"jpe" => "image/jpeg",
+"jpeg" => "image/jpeg",
+"jpg" => "image/jpeg",
+"js" => "application/x-javascript",
+"latex" => "application/x-latex",
+"lha" => "application/octet-stream",
+"lsf" => "video/x-la-asf",
+"lsx" => "video/x-la-asf",
+"lzh" => "application/octet-stream",
+"m13" => "application/x-msmediaview",
+"m14" => "application/x-msmediaview",
+"m3u" => "audio/x-mpegurl",
+"man" => "application/x-troff-man",
+"mdb" => "application/x-msaccess",
+"me" => "application/x-troff-me",
+"mht" => "message/rfc822",
+"mhtml" => "message/rfc822",
+"mid" => "audio/mid",
+"mny" => "application/x-msmoney",
+"mov" => "video/quicktime",
+"movie" => "video/x-sgi-movie",
+"mp2" => "video/mpeg",
+"mp3" => "audio/mpeg",
+"mpa" => "video/mpeg",
+"mpe" => "video/mpeg",
+"mpeg" => "video/mpeg",
+"mpg" => "video/mpeg",
+"mpp" => "application/vnd.ms-project",
+"mpv2" => "video/mpeg",
+"ms" => "application/x-troff-ms",
+"mvb" => "application/x-msmediaview",
+"nws" => "message/rfc822",
+"oda" => "application/oda",
+"p10" => "application/pkcs10",
+"p12" => "application/x-pkcs12",
+"p7b" => "application/x-pkcs7-certificates",
+"p7c" => "application/x-pkcs7-mime",
+"p7m" => "application/x-pkcs7-mime",
+"p7r" => "application/x-pkcs7-certreqresp",
+"p7s" => "application/x-pkcs7-signature",
+"pbm" => "image/x-portable-bitmap",
+"pdf" => "application/pdf",
+"pfx" => "application/x-pkcs12",
+"pgm" => "image/x-portable-graymap",
+"pko" => "application/ynd.ms-pkipko",
+"pma" => "application/x-perfmon",
+"pmc" => "application/x-perfmon",
+"pml" => "application/x-perfmon",
+"pmr" => "application/x-perfmon",
+"pmw" => "application/x-perfmon",
+"pnm" => "image/x-portable-anymap",
+"pot" => "application/vnd.ms-powerpoint",
+"ppm" => "image/x-portable-pixmap",
+"pps" => "application/vnd.ms-powerpoint",
+"ppt" => "application/vnd.ms-powerpoint",
+"prf" => "application/pics-rules",
+"ps" => "application/postscript",
+"pub" => "application/x-mspublisher",
+"qt" => "video/quicktime",
+"ra" => "audio/x-pn-realaudio",
+"ram" => "audio/x-pn-realaudio",
+"ras" => "image/x-cmu-raster",
+"rgb" => "image/x-rgb",
+"rmi" => "audio/mid",
+"roff" => "application/x-troff",
+"rtf" => "application/rtf",
+"rtx" => "text/richtext",
+"scd" => "application/x-msschedule",
+"sct" => "text/scriptlet",
+"setpay" => "application/set-payment-initiation",
+"setreg" => "application/set-registration-initiation",
+"sh" => "application/x-sh",
+"shar" => "application/x-shar",
+"sit" => "application/x-stuffit",
+"snd" => "audio/basic",
+"spc" => "application/x-pkcs7-certificates",
+"spl" => "application/futuresplash",
+"src" => "application/x-wais-source",
+"sst" => "application/vnd.ms-pkicertstore",
+"stl" => "application/vnd.ms-pkistl",
+"stm" => "text/html",
+"svg" => "image/svg+xml",
+"sv4cpio" => "application/x-sv4cpio",
+"sv4crc" => "application/x-sv4crc",
+"t" => "application/x-troff",
+"tar" => "application/x-tar",
+"tcl" => "application/x-tcl",
+"tex" => "application/x-tex",
+"texi" => "application/x-texinfo",
+"texinfo" => "application/x-texinfo",
+"tgz" => "application/x-compressed",
+"tif" => "image/tiff",
+"tiff" => "image/tiff",
+"tr" => "application/x-troff",
+"trm" => "application/x-msterminal",
+"tsv" => "text/tab-separated-values",
+"txt" => "text/plain",
+"" => "text/plain",
+"uls" => "text/iuls",
+"ustar" => "application/x-ustar",
+"vcf" => "text/x-vcard",
+"vrml" => "x-world/x-vrml",
+"wav" => "audio/x-wav",
+"wcm" => "application/vnd.ms-works",
+"wdb" => "application/vnd.ms-works",
+"wks" => "application/vnd.ms-works",
+"wmf" => "application/x-msmetafile",
+"wps" => "application/vnd.ms-works",
+"wri" => "application/x-mswrite",
+"wrl" => "x-world/x-vrml",
+"wrz" => "x-world/x-vrml",
+"xaf" => "x-world/x-vrml",
+"xbm" => "image/x-xbitmap",
+"xla" => "application/vnd.ms-excel",
+"xlc" => "application/vnd.ms-excel",
+"xlm" => "application/vnd.ms-excel",
+"xls" => "application/vnd.ms-excel",
+"xlsx" => "application/vnd.ms-excel",
+"xlt" => "application/vnd.ms-excel",
+"xlw" => "application/vnd.ms-excel",
+"xof" => "x-world/x-vrml",
+"xpm" => "image/x-xpixmap",
+"xwd" => "image/x-xwindowdump",
+"z" => "application/x-compress",
+"zip" => "application/zip");					
+
+?>
