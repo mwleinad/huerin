0d317dc964073e047e989695c1bb7545a3200184
dohko01 <fzero_69@hotmail.com>
2017-06-11 11:06:58 -0500

    Scripts

---
+++ updateTaskFiles.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+#encoding: UTF-8
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+import os
+
+files = ""
+filesSplit = ""
+daFile = ""
+
+files = os.popen("find ../sistema/ -ctime -5").read()
+
+filesSplit = [s.strip() for s in files.splitlines()]
+
+for xx in filesSplit:
+    if(os.path.isfile(xx)):
+        resultado = os.popen("/usr/bin/php /var/www/html/cron/updateTaskFile.php "+"'"+xx+"'").read()
+        print resultado
