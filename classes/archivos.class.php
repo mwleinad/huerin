75663d771e8756cf7567ea686b000c3c1a488474
dohko01 <fzero_69@hotmail.com>
2017-06-11 16:07:29 -0500

    Test

--- classes/archivos.class.php
+++ classes/archivos.class.php
@@ -54,7 +54,7 @@ class Archivos extends Servicio {
                     }
                 }
                 if($this->checkDir($this->FILES_ROOT . $contract['rfc'] . "/" . $instancia['dateExploded'][0] . "/" . $instancia['dateExploded'][1] . "/" . $instancia['instanciaServicioId'] . "_" . $instancia['nombreServicio'])){
-                    $query = "UPDATE instanciaServicio SET carpeta = 1 WHERE instanciaServicioId = " . $instancia['instanciaServicioId'];
+                    echo $query = "UPDATE instanciaServicio SET carpeta = 1 WHERE instanciaServicioId = " . $instancia['instanciaServicioId'];
                     $this->Util()->DB()->setQuery($query);
                 }
             }
