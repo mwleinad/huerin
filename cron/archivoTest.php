31d320b9641a9e90b1aa36120577ba8ab09e63d0
dohko01 <fzero_69@hotmail.com>
2017-06-12 18:02:03 -0500

    Test

--- cron/archivoTest.php
+++ cron/archivoTest.php
@@ -1,19 +1,19 @@
 <?php
-//exit;
-//if(!$_SERVER["DOCUMENT_ROOT"])
-//{
-//    $_SERVER["DOCUMENT_ROOT"] = realpath(dirname(__FILE__).'/..');
-//}
-//
-//if($_SERVER['DOCUMENT_ROOT'] != "/var/www/html")
-//{
-//	$docRoot = $_SERVER['DOCUMENT_ROOT'];
-//}
-//else
-//{
-//	$docRoot = $_SERVER['DOCUMENT_ROOT'];
-//}
-//
+
+if(!$_SERVER["DOCUMENT_ROOT"])
+{
+    $_SERVER["DOCUMENT_ROOT"] = realpath(dirname(__FILE__).'/..');
+}
+
+if($_SERVER['DOCUMENT_ROOT'] != "/var/www/html")
+{
+	$docRoot = $_SERVER['DOCUMENT_ROOT'];
+}
+else
+{
+	$docRoot = $_SERVER['DOCUMENT_ROOT'];
+}
+
 	$docRoot = $_SERVER['DOCUMENT_ROOT'];
 	define('DOC_ROOT', $docRoot."/dev/huerin");
 
