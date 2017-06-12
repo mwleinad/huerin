3afc08c2afa43b273ec900c78b2fb74c5da5e67a
dohko01 <fzero_69@hotmail.com>
2017-06-11 19:12:12 -0500

    Test

--- cron/updateTaskFile.php
+++ cron/updateTaskFile.php
@@ -2,6 +2,24 @@
 
 error_reporting(0);
 
+if(!$_SERVER["DOCUMENT_ROOT"])
+        $_SERVER["DOCUMENT_ROOT"] = realpath(dirname(__FILE__).'/..');
+
+if($_SERVER['DOCUMENT_ROOT'] != "/var/www/html")
+        $docRoot = $_SERVER['DOCUMENT_ROOT']."/huerin_test";
+else
+        $docRoot = $_SERVER['DOCUMENT_ROOT'];
+
+define('DOC_ROOT', $docRoot);
+
+echo DOC_ROOT; exit(0);
+
+session_save_path("/tmp");
+
+include_once(DOC_ROOT.'/init.php');
+include_once(DOC_ROOT.'/config.php');
+include_once(DOC_ROOT.'/libraries.php');
+
 $docs = "/var/www/";
 
 $argv = $_SERVER['argv'];
--- updateTaskFiles.py
+++ updateTaskFiles.py
@@ -5,16 +5,23 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 import os
+import time
 
-files = ""
-filesSplit = ""
-daFile = ""
+def executeSomething():
+    #code here
+    files = ""
+    filesSplit = ""
+    daFile = ""
 
-files = os.popen("find /var/www/sistema/ -ctime -5").read()
+    files = os.popen("find /var/www/sistema/ -cmin -20").read()
 
-filesSplit = [s.strip() for s in files.splitlines()]
+    filesSplit = [s.strip() for s in files.splitlines()]
 
-for xx in filesSplit:
-    if(os.path.isfile(xx)):
-        resultado = os.popen("/usr/bin/php /var/www/html/dev/huerin/cron/updateTaskFile.php "+"'"+xx+"'").read()
-        print resultado
+    for xx in filesSplit:
+        if(os.path.isfile(xx)):
+            os.popen("/usr/bin/php /var/www/html/dev/huerin/cron/updateTaskFile.php "+"'"+xx+"'").read()
+            
+    time.sleep(36000)
+
+while True:
+    executeSomething()
