b1a04695c9056ae56089e6e8caf317cd344dd2ad
dohko01 <fzero_69@hotmail.com>
2017-06-14 20:33:30 -0500

    Test inotify

--- cron/updateTaskFile.php
+++ cron/updateTaskFile.php
@@ -20,7 +20,7 @@ $docs = "/var/www/";
 
 $argv = $_SERVER['argv'];
 
-$filename = $argv[1];
+echo $filename = $argv[1];
 
 if (is_file($filename)) {
     $fileUpdated = date("Y/m/d H:i:s.", filemtime($filename));
