243b17a3ed1777f90242274bf87874f742ef70fd
dohko01 <fzero_69@hotmail.com>
2017-07-10 18:10:09 -0500

    Test

--- classes/notice.class.php
+++ classes/notice.class.php
@@ -85,11 +85,18 @@ class Notice extends Main
 	}
 	
 	public function Save(){
-                $localIPmenos53 = getHostByName(php_uname('n'));
-                $localIPmas53 = getHostByName(getHostName());
-                
-                echo $localIPmenos53;
-                echo "<br>".$localIPmas53;
+            
+                foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+                    if (array_key_exists($key, $_SERVER) === true){
+                        foreach (explode(',', $_SERVER[$key]) as $ip){
+                            $ip = trim($ip); // just to be safe
+
+                            if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                                echo $ip;
+                            }
+                        }
+                    }
+                }
                 
                 exit(0); 
                  
