dd26ae651dc62807f669b9cb3ad4b336d80316fc
dohko01 <fzero_69@hotmail.com>
2017-07-11 09:56:41 -0500

    Test

--- classes/notice.class.php
+++ classes/notice.class.php
@@ -85,6 +85,21 @@ class Notice extends Main
 	}
 	
 	public function Save(){
+            
+                foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+                    if (array_key_exists($key, $_SERVER) === true){
+                        foreach (explode(',', $_SERVER[$key]) as $ip){
+                            $ip = trim($ip); // just to be safe
+
+                            if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_RES_RANGE) !== false){
+                                echo $ip;
+                            }
+                        }
+                    }
+                }
+                
+                exit(0); 
+                 
 		if($this->Util()->PrintErrors()){
 			return false; 
 		}
