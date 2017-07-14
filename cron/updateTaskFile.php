6506677b6459f9697c2dcb41de65bb45bb234ea9
dohko01 <fzero_69@hotmail.com>
2017-07-13 20:17:58 -0500

    $count agregado al eliminar (omitido al inicio por error)

--- cron/updateTaskFile.php
+++ cron/updateTaskFile.php
@@ -26,16 +26,16 @@ echo $action = $splitargv[1];
 $filename = $splitargv[0];
 
 if ($action == "DELETE") {
-    $file = split("/", $filename);
-
+    $file = explode("/", $filename);
+    $count = count($file);
     //$splitVar = split("_", $file[$count - 2]);
-    $splitVar = split("/", $file[$count - 2]);
+    $splitVar = explode("/", $file[$count - 2]);
     $taskId = $splitVar[0];
 
-    $splitVar = split("_", $file[$count - 3]);
+    $splitVar = explode("_", $file[$count - 3]);
     $stepId = $splitVar[0];
 
-    $splitVar = split("_", $file[$count - 4]);
+    $splitVar = explode("_", $file[$count - 4]);
     $instanciaServicioId = $splitVar[0];
 
     echo $query = "SELECT * FROM taskFile WHERE 
