7d5ae56bf6baaecd722b442101b62696cf03dee9
dohko01 <fzero_69@hotmail.com>
2017-07-15 21:53:11 -0500

    Test

--- download.php
+++ download.php
@@ -14,15 +14,15 @@ $ext = @strtolower(end(explode('.', $_GET["file"])));
   }
  */
 
-$mime = $mime_types[$ext];
-$file = explode("/", $_GET["file"]);
-header('Content-Disposition: attachment; filename=' . @end($file));
-header('Content-type:' . $mime);
-//readfile(urldecode($_GET["file"]));
-$_GET["file"] = str_replace(WEB_ROOT, "", $_GET["file"]);
-$file = DOC_ROOT . "/" . $_GET["file"];
-
-if (!is_file($file)) {
+if (is_file($file)){
+    $mime = $mime_types[$ext];
+    $file = explode("/", $_GET["file"]);
+    header('Content-Disposition: attachment; filename=' . @end($file));
+    header('Content-type:' . $mime);
+    //readfile(urldecode($_GET["file"]));
+    $_GET["file"] = str_replace(WEB_ROOT, "", $_GET["file"]);
+    $file = DOC_ROOT . "/" . $_GET["file"];
+}else{
 
     $explodedRoute = explode("/", $file);
     $explodedFileName = explode("_", $explodedRoute[1]);
