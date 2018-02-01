9569eb83232188ea8b0f137a6d7111db384e4ecd
Arista Soluciones <desarrollo@aristasoluciones.com>
2018-02-01 16:01:57 -0600

    correcion de tipo de regimen de Jacobo Braun

--- cron-nuevo/rep-fiel.php
+++ cron-nuevo/rep-fiel.php
@@ -28,3 +28,25 @@ $sql = "SELECT * FROM personal WHERE tipoPersonal NOT IN('socio','asistente')
         AND (lastSendEmail < DATE(NOW()) OR lastSendEmail IS NULL) ORDER BY personalId ASC LIMIT 3";
 $db->setQuery($sql);
 $employees = $db->GetResult($sql);
+
+foreach($employees as $key=>$itemEmploye) {
+    if (!$util->ValidateEmail(trim($itemEmploye['email']))) {
+        echo $itemEmploye['personalId'] . " correo no valido : " . $itemEmploye['email'];
+        echo "<br>";
+        $up = 'UPDATE personal SET lastSendEmail=" ' . date("Y-m-d") . ' " WHERE personalId=' . $itemEmploye["personalId"] . ' ';
+        $db->setQuery($up);
+        $db->UpdateData();
+        continue;
+    }
+    $deptos = array();
+    $persons = array();
+    $personal->setPersonalId($itemEmploye['personalId']);
+    $subordinados = $personal->Subordinados(true);
+    $persons = $util->ConvertToLineal($subordinados, 'personalId');
+    $deptos = $util->ConvertToLineal($subordinados, 'dptoId');
+
+    array_unshift($persons, $itemEmploye['personalId']);
+    array_unshift($deptos, $itemEmploye['departamentoId']);
+    $contracts = $contractRep->BuscarContractV2($persons, true, $deptos);
+
+}
