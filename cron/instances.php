4004fd6b5200d24279d28f6975c41c075257e028
Arista Soluciones <isc061990@outlook.com>
2018-04-03 17:02:22 -0500

    d

--- cron/instances.php
+++ cron/instances.php
@@ -16,7 +16,7 @@ else
 
 	define('DOC_ROOT', $docRoot);
 
-	session_save_path("C:/xampp/tmp2");
+	session_save_path("/tmp");
 
 	include_once(DOC_ROOT.'/init_cron.php');
 	include_once(DOC_ROOT.'/config.php');
