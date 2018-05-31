8f4d2e65681cdc38e86821f386c3659f2363816d
Arista Soluciones <isc061990@outlook.com>
2018-05-31 14:15:43 -0500

    Ingresar unicamente los costos de los servicios de Contabilidad y Nominas (Nomina, IMSS e ISN) activo en el reporte de Razones Sociales.

--- classes/contract.class.php
+++ classes/contract.class.php
@@ -2902,7 +2902,8 @@ class Contract extends Main
         return $data;
     }
     public function getTotalIguala(){
-      $this->Util()->DBSelect($_SESSION['empresaId'])->setQuery('SELECT SUM(costo) FROM servicio WHERE contractId="'.$this->contractId.'" AND status="activo" ');
+      $sql = "SELECT SUM(a.costo) FROM servicio a INNER JOIN tiposervicio b ON a.tipoServicioId=b.tipoServicioId  AND b.status='1' WHERE a.contractId='".$this->contractId."' AND a.status='activo' AND b.departamentoId IN(1,24)";
+      $this->Util()->DBSelect($_SESSION['empresaId'])->setQuery($sql);
       $single =  $this->Util()->DBSelect($_SESSION['empresaId'])->GetSingle();
       return $single;
     }
