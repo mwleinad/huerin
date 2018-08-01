a2eb9fdc88cdf8007de8179db4606e52dc3f43dc
Arista Soluciones <isc061990@outlook.com>
2018-08-01 14:30:58 -0500

    correccion minima

--- classes/instanciaServicio.class.php
+++ classes/instanciaServicio.class.php
@@ -31,7 +31,7 @@ class InstanciaServicio extends  Servicio
 				AND (servicio.status != 'baja'
       			OR servicio.status != 'inactiva')
 				AND instanciaServicio.status != 'baja'		
-				AND servicio.servicioId = '".$servicioId."' GROUP BY MONTH(instanciaServicio.date) ORDER BY instanciaServicio.date ASC " ;
+				AND servicio.servicioId = '".$servicioId."'  ORDER BY instanciaServicio.instanciaServicioId DESC" ;
         $this->Util()->DB()->setQuery($sql);
         $data = $this->Util()->DB()->GetResult();
 
