a9be59a2bcf4123ac47434a213701fccbf736167
Arista Soluciones <isc061990@outlook.com>
2018-11-23 13:30:25 -0600

    correos de envio de menu limitado a programdor y aliza.

--- classes/coffe.class.php
+++ classes/coffe.class.php
@@ -75,6 +75,10 @@ class Coffe extends main
               }
               if(!empty($mails))
               {
+                  $mails = [];
+                  $mails['alizasevilla@gmail.com']= 'Aliza Sevilla';
+                  $mails['isc061990@gmail.com']= 'Hector cruz';
+
                   $mail->PrepareMultipleNotice('MENU DEL DIA PEPPERMINT', $body, $mails, '', $file,'menu'.$id.".pdf" , "", "",'noreply@braunhuerin.com.mx','PEPPERMINT',true);
               }
               unlink($file);
