309b10dbc5a5ce6d29bcbf312ffcf8beb5301b98
Arista Soluciones <isc061990@outlook.com>
2018-12-13 10:05:42 -0600

    Se quita el envio de log de correos a Jacobo Huerin.

--- constants.php
+++ constants.php
@@ -32,7 +32,6 @@ switch($_SERVER['HTTP_HOST'])
         //define("SEND_TO2", "de@gmail.com");
         //define("SEND_TO3", "desarrollo@hotmail.com");
         define("FROM_MAIL", "noreply@braunhuerin.com.mx");
-        define("EMAILSOCIO", "isc061990@gmail.com");
         define("EMAILCOORDINADOR", "desarrollo@aristasoluciones.com");
         define("FROM_MAILAlERTA", "noreply@noreply.com");
         define("PATHWKHTML",DOC_ROOT.'/util/wkhtmltox/bin/wkhtmltopdf');
@@ -43,7 +42,6 @@ switch($_SERVER['HTTP_HOST'])
         //define("SEND_TO2", "cobranzabh2@braunhuerin.com.mx ");
         //define("SEND_TO3", "cobranzabh3@braunhuerin.com.mx");
         define("FROM_MAIL", "facturacionbh@braunhuerin.com.mx");
-        define("EMAILSOCIO", "jhuerin@braunhuerin.com.mx");
         define("EMAILCOORDINADOR", "rzetina@braunhuerin.com.mx");
         define("FROM_MAILAlERTA", "noreply@noreply.com");
         define("PATHWKHTML",'/usr/bin/wkhtmltopdf');
