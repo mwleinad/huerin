ae216f7d59aa9808b11aed0cd80876efa2bfc0a5
Arista Soluciones <isc061990@outlook.com>
2019-01-22 20:25:35 -0600

    .

--- modules/backup_system.php
+++ modules/backup_system.php
@@ -1,6 +1,6 @@
 <?php
 $user->allowAccess(217);
-//$user->allowAccess(214);
+$user->allowAccess(218);
 /* End Session Control */
 //clean session platillos if exist
 $bd_user= SQL_USER;
@@ -8,7 +8,8 @@ $bd_host = SQL_HOST;
 $bd_pass = SQL_PASSWORD;
 
 $file = DOC_ROOT.'/sendFiles/list_bd.txt';
-
+echo 'mysql -h'.$bd_host.' -u'.$bd_user.' -p'.$bd_pass.' -e "show databases where `Database` not in(\'mysql\',\'phpmyadmin\',\'information_schema\',\'performance_schema\') " >'.$file;
+exit;
 exec('mysql -h'.$bd_host.' -u'.$bd_user.' -p'.$bd_pass.' -e "show databases where `Database` not in(\'mysql\',\'phpmyadmin\',\'information_schema\',\'performance_schema\') " >'.$file,$de);
 $listDatabases = [];
 if(file_exists($file)){
