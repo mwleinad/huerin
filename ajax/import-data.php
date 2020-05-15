60eb3911f9609e9c6741b752703dfef97b09c55f
aristasoluciones <isc061990@outlook.com>
2020-05-14 20:04:51 -0500

    correcciones al importar nuevos registros.

--- ajax/import-data.php
+++ ajax/import-data.php
@@ -153,9 +153,7 @@ switch ($opcion[0]) {
                 }
 
             }
-
-            echo $sql = "insert into $table ($fields_string) VALUES ($values_string)";
-            continue;
+            $sql = "insert into $table ($fields_string) VALUES ($values_string)";
             $db_connection->setQuery($sql);
             $id = $db_connection->InsertData();
 
