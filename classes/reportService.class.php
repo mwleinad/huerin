3e62f983cdc12cb52ee085caefb8ddffb4d64fad
aristasoluciones <isc061990@outlook.com>
2020-11-19 03:08:16 -0600

    de

--- classes/reportService.class.php
+++ classes/reportService.class.php
@@ -112,67 +112,6 @@ class ReportService extends Servicio {
                 }
 
             }
-            /*
-            $firstHistory =$history[key($history)];
-            end($history);
-            $lastHistory = $history[key($history)];
-
-            $currentStatus = $var['status'];
-            $dateAlta =  $firstHistory['fecha'];
-            $dateBaja =  null;
-            if (in_array($currentStatus, $statusBaja)) {
-                $dateBaja = $this->Util()->isValidateDate($var['fechaBaja'], 'Y-m-d') ? $var['fechaBaja'] :  $lastHistory ['fecha'];
-            }
-
-
-            $dateAlta = date('Y-m-d', strtotime($dateAlta));
-            $dateBaja = date('Y-m-d', strtotime($dateBaja));
-            $monthDateAlta = date('m', strtotime($dateAlta));
-            $var['month'] =  $monthsComplete[$monthDateAlta];
-            $var['dateMovimiento'] =  $firstHistory['fecha'];
-            $var['typeMovimiento'] =  'Alta';
-
-            if($_POST['statusSearch'] == 'baja' || $_POST['statusSearch'] == 'alta') {
-
-                if ($_POST['statusSearch'] == 'baja') {
-                    $dateEval = $dateBaja;
-                    $var['dateMovimiento'] =  $lastHistory['fecha'];
-                    $var['typeMovimiento'] =  'Baja';
-                } elseif ($_POST['statusSearch'] == 'alta') {
-                    $dateEval = $dateAlta;
-                    $var['dateMovimiento'] =  $firstHistory['fecha'];
-                }
-
-                $dateMonth = date('m', strtotime($dateEval));
-                $dateYear = (int) date('Y', strtotime($dateEval));
-
-                if ($filterYear) {
-                    if ($filterYear !== $dateYear)
-                        $flag = false;
-                }
-
-                if ($filterMonth) {
-                    if ($filterMonth !== (int)$dateMonth)
-                        $flag = false;
-                }
-                $var['month'] =  $monthsComplete[$dateMonth];
-            }
-
-
-            if ($flag) {
-                $sql = ' SELECT a.departamentoId, a.departamento, b.personalId, c.name  FROM departamentos a
-                         LEFT JOIN contractPermiso b ON a.departamentoId=b.departamentoId
-                         INNER JOIN personal c ON b.personalId = c.personalId
-                         WHERE b.contractId = "'.$var['contractId'].'"
-                         GROUP BY a.departamentoId';
-
-                $this->Util()->DB()->setQuery($sql);
-                $responsables = $this->Util()->DB()->GetResult();
-                $key =  array_search($var['departamentoId'], array_column($responsables, 'departamentoId'));
-                $var['supervisor']  = $personal->findSupervisor($responsables[$key]['personalId']);
-                array_push($items, $var);
-
-            }*/
         }
         return $items;
     }
