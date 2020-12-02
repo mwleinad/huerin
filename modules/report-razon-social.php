46056099f8dd3503d764560078a438f4343a058f
aristasoluciones <isc061990@outlook.com>
2020-12-02 09:58:56 -0600

    modifcacion minima

--- modules/report-razon-social.php
+++ modules/report-razon-social.php
@@ -1,6 +1,6 @@
 <?php
 $user->allowAccess(7);  //level 1
-$user->allowAccess(165);//level 2
+$user->allowAccess(167);//level 2
 
 $personals = $personal->Enumerate();
 $smarty->assign("personals", $personals);
