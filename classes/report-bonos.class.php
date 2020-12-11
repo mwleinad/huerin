15b4668e96cf3b9e9280d7c43c8e5166d2907f15
aristasoluciones <isc061990@outlook.com>
2020-12-11 08:27:35 -0600

    debug 6

--- classes/report-bonos.class.php
+++ classes/report-bonos.class.php
@@ -616,10 +616,10 @@ class ReporteBonos extends Main
                         $totales["totalEmpresas"]++;
                     }
                     foreach($service["instancias"] as $ikey=>$itemins){
-                        $serviciosEncontrados[$socio]['subordinados'][$gerente]['subordinados'][$subgerente]["subordinados"][$supervisor]["subordinados"][$encargadoDep]['totalVerticalDevengado'][$ikey] +=$itemins['costo'];
-                        $serviciosEncontrados[$socio]['subordinados'][$gerente]['subordinados'][$subgerente]["subordinados"][$supervisor]["subordinados"][$encargadoDep]['totalVerticalCompletado'][$ikey] +=$itemins['completado'];
-                        $totales['granTotalVerticalDevengado'][$ikey] += $itemins['costo'];
-                        $totales['granTotalVerticalCompletado'][$ikey] += $itemins['completado'];
+                        @$serviciosEncontrados[$socio]['subordinados'][$gerente]['subordinados'][$subgerente]["subordinados"][$supervisor]["subordinados"][$encargadoDep]['totalVerticalDevengado'][$ikey] +=@$itemins['costo'];
+                        @$serviciosEncontrados[$socio]['subordinados'][$gerente]['subordinados'][$subgerente]["subordinados"][$supervisor]["subordinados"][$encargadoDep]['totalVerticalCompletado'][$ikey] +=@$itemins['completado'];
+                        @$totales['granTotalVerticalDevengado'][$ikey] += @$itemins['costo'];
+                        @$totales['granTotalVerticalCompletado'][$ikey] += @$itemins['completado'];
 
                         if(!in_array($encargadoDep,$listsEncargados)){
                             array_push($listsEncargados,$encargadoDep);
@@ -631,8 +631,8 @@ class ReporteBonos extends Main
                             $cad['personalId']=$encargadoDep;
                             $totalesEncargados[$encargadoDep]=$cad;
                         }else{
-                            $totalesEncargados[$encargadoDep]['totalDevengado']+=$itemins['costo'];
-                            $totalesEncargados[$encargadoDep]['totalCompletado']+=$itemins['completado'];
+                            $totalesEncargados[$encargadoDep]['totalDevengado']+=@$itemins['costo'];
+                            $totalesEncargados[$encargadoDep]['totalCompletado']+=@$itemins['completado'];
                         }
                     }
                 break;
@@ -711,9 +711,9 @@ class ReporteBonos extends Main
             }
         } else {
             $newArray[$value["jefeInmediato"]] = $allEncargados[$value["jefeInmediato"]];
-            $newArray[$value['jefeInmediato']]['totalDevengado']  += $acumDevengado;
-            $newArray[$value['jefeInmediato']]['totalCompletado'] += $acumCompletado;
-            $newArray[$value['jefeInmediato']]['sueldoTotal'] += $sueldo;
+            @$newArray[$value['jefeInmediato']]['totalDevengado']  += $acumDevengado;
+            @$newArray[$value['jefeInmediato']]['totalCompletado'] += $acumCompletado;
+            @$newArray[$value['jefeInmediato']]['sueldoTotal'] += $sueldo;
             $sueldo = $newArray[$value['jefeInmediato']]["sueldo"] + $sueldo;
             if ($newArray[$value['jefeInmediato']]['jefeInmediato']) {
                 $this->recursiveTotalEncargado($allEncargados, $newArray, $newArray[$value['jefeInmediato']], $acumDevengado, $acumCompletado, $sueldo);
--- export/report-bonos.php
+++ export/report-bonos.php
@@ -4,13 +4,13 @@ if(!isset($_SESSION)){
 }
 
 include_once('../config.php');
-/*ini_set("display_errors", 1);
+ini_set("display_errors", 1);
 error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED & ~E_NOTICE ^ E_WARNING);
 if (PHP_MAJOR_VERSION >= 7) {
     set_error_handler(function ($errno, $errstr) {
         return strpos($errstr, 'Declaration of') === 0;
     }, E_WARNING);
-}*/
+}
 date_default_timezone_set('America/Mexico_City');
 include_once(DOC_ROOT.'/libraries.php');
 
@@ -66,4 +66,5 @@ header("Content-type:   application/x-msexcel; charset=utf-8");
 header("Content-Disposition: attachment; filename=".$name.".xls");
 header("Pragma: no-cache");
 header("Expires: 0");
+echo $html;
 exit;
