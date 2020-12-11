6324be8843f72ddb35a01bb81a6b3e7284a8ad00
aristasoluciones <isc061990@outlook.com>
2020-12-11 08:16:46 -0600

    debug 4

--- export/report-bonos.php
+++ export/report-bonos.php
@@ -4,18 +4,17 @@ if(!isset($_SESSION)){
 }
 
 include_once('../config.php');
-ini_set("display_errors", 1);
+/*ini_set("display_errors", 1);
 error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED & ~E_NOTICE ^ E_WARNING);
 if (PHP_MAJOR_VERSION >= 7) {
     set_error_handler(function ($errno, $errstr) {
         return strpos($errstr, 'Declaration of') === 0;
     }, E_WARNING);
-}
+}*/
 date_default_timezone_set('America/Mexico_City');
 include_once(DOC_ROOT.'/libraries.php');
 
 $data = $reportebonos->generateReportBonosWhitLevel($_POST);
-dd($data);
 $period = $_POST['period'];
 if($period== "efm"){
     $monthNames = array("Ene", "Feb", "Mar");
@@ -60,7 +59,6 @@ $smarty->assign("data", $data);
 $smarty->assign("EXCEL", "SI");
 $smarty->assign("DOC_ROOT", DOC_ROOT);
 $html .= $smarty->fetch(DOC_ROOT.'/templates/lists/report-servicio-bono-order-rol.tpl');
-echo $html;exit;
 $name = 'reporte_de_bonos_cobranza';
 ob_clean();
 header("Content-Type:   application/vnd.ms-excel; charset=utf-8");
@@ -68,5 +66,4 @@ header("Content-type:   application/x-msexcel; charset=utf-8");
 header("Content-Disposition: attachment; filename=".$name.".xls");
 header("Pragma: no-cache");
 header("Expires: 0");
-echo $html;
 exit;
