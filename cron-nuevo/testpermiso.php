d9c58f56b64aa19a55f21b916739eb68110d0b41
aristasoluciones <isc061990@outlook.com>
2020-12-17 23:07:39 -0600

    cambios en cron

--- cron-nuevo/testpermiso.php
+++ cron-nuevo/testpermiso.php
@@ -1,22 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: RAGNAR
- * Date: 30/04/2018
- * Time: 06:37 PM
- */
-//si el dia que se ejecuta es mayor que los dias 25 de cada mes se anula
-if(date('d')>=25)
-{
-    echo "ejecutado en los ultimos dias de mes, se anula tarea".chr(13);
-    exit;
-}
 if(!$_SERVER["DOCUMENT_ROOT"])
 {
     $_SERVER["DOCUMENT_ROOT"] = realpath(dirname(__FILE__).'/..');
 }
 
-if($_SERVER['DOCUMENT_ROOT'] != "/var/www/html")
+if($_SERVER['DOCUMENT_ROOT'] != "/var/www/html" && $_SERVER['DOCUMENT_ROOT'] != "/var/www/mainplatform/public_html")
 {
     $docRoot = $_SERVER['DOCUMENT_ROOT']."/huerin";
     session_save_path("C:/xampp/tmp");
@@ -38,21 +26,6 @@ if (!isset($_SESSION))
 }
 $current =  date("Y-m-d");
 $firstDay =  $util->getFirstDate($current);
-if($current==$firstDay)
-{
-    if(strtotime(date('H:i:s'))<strtotime('02:30:00')){
-        echo "ejecutado antes de las 02:30:00 del dia ".$firstDay.chr(13);
-        exit;
-    }
-}
-$_SESSION['empresaId'] = IDEMPRESA;
-$mask = DOC_ROOT.'/temp/15_A_*.*';
-$array = glob($mask);
-array_map('unlink', glob($mask));
-$mask = DOC_ROOT.'/temp/20_B_*.*';
-$array = glob($mask);
-array_map('unlink', glob($mask));
-$entry =  " inicio ". date("Y-m-d H:i:s").chr(13).chr(10);
-$invoiceService->GenerateInvoices();
-$entry  .=" fin ". date("Y-m-d H:i:s").chr(13).chr(10);
-echo $entry;
+
+echo "permiso para eejcutr correcto ".$firstDay."\n";
+
