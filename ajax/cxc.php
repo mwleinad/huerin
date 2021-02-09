eef5476a34efbf0661b8899e6c5ad2325cc3c3cc
aristasoluciones <isc061990@outlook.com>
2021-02-09 15:42:10 -0600

    cambiar libraries por libraries33 en cxc

--- ajax/cxc.php
+++ ajax/cxc.php
@@ -2,7 +2,7 @@
 
 include_once('../init.php');
 include_once('../config.php');
-include_once(DOC_ROOT . '/libraries.php');
+include_once(DOC_ROOT . '/libraries33.php');
 
 switch ($_POST['type']) {
 
