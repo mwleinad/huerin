05ffedc34dbaca6f79a658efd4a12a13a56b9c4d
aristasoluciones <isc061990@outlook.com>
2021-04-21 17:51:55 -0500

    de

--- init_files.php
+++ init_files.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 if (!isset($_SESSION)){
   session_start();
 }
 
 ini_set("display_errors", "ON");
-error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_WARNING);
+//error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_WARNING);
 
 date_default_timezone_set('America/Mexico_City');
 header('Content-type: text/html; charset=iso-8859-1');
