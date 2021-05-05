5ecb4a6d61c6316ed6a64d576824d68027b248a6
aristasoluciones <isc061990@outlook.com>
2021-05-04 19:07:19 -0500

    cambios para realizar backups

--- cron-nuevo/backup-db.php
+++ cron-nuevo/backup-db.php
@@ -4,14 +4,7 @@ if(!$_SERVER["DOCUMENT_ROOT"])
 {
     $_SERVER["DOCUMENT_ROOT"] = realpath(dirname(__FILE__).'/..');
 }
-if($_SERVER['DOCUMENT_ROOT'] != "/var/www/mainplatform/public_html")
-{
-    $docRoot = $_SERVER['DOCUMENT_ROOT']."/huerin";
-}
-else
-{
-    $docRoot = $_SERVER['DOCUMENT_ROOT'];
-}
+$docRoot = $_SERVER['DOCUMENT_ROOT'];
 define('DOC_ROOT', $docRoot);
 include_once(DOC_ROOT.'/init.php');
 include_once(DOC_ROOT.'/config.php');
