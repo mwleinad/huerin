1b852c9c5d945630226d84187b3982c4fe61bca1
aristasoluciones <isc061990@outlook.com>
2021-05-06 13:57:35 -0500

    actualizar path en tabla documento , faltantes.

--- cron-nuevo/rutasdoc.php
+++ cron-nuevo/rutasdoc.php
@@ -10,10 +10,7 @@ include_once(DOC_ROOT.'/init.php');
 include_once(DOC_ROOT.'/config.php');
 include_once(DOC_ROOT.'/libraries.php');
 
-echo "Cron sin funcion";
-exit;
-
-$file_temp = DOC_ROOT."/documento.csv";
+$file_temp = DOC_ROOT."/manualupdatepathdoc.csv";
 $fp = fopen($file_temp,'r');
 $fila= 1;
 $update = 0;
@@ -23,17 +20,16 @@ while(($row=fgetcsv($fp,4096,","))==true) {
         $fila++;
         continue;
     }
-    $name_file =  $row[1]."_".$row[4];
-    if(is_file(DOC_ROOT."/documentos/".$name_file)) {
-        $sql = "update documento set path = '".$row[4]."' where documentoId = '".$row[0]."' ";
+        $sql = "update documento set path = '".$row[1]."' where documentoId = '".$row[0]."' ";
         $db->setQuery($sql);
         $row = $db->UpdateData();
-        if($row)
+        if($row) {
             $update++;
-    } else {
-        $noupdate++;
-        echo $row[0].",".$row[1].",".$row[2].",".$row[3].",".$row[4].chr(10).chr(13)."<br>";
-    }
+        } else {
+            $noupdate++;
+        }
+        echo $sql." <br />";
+
     $fila++;
 }
 echo "Total actualizado".$update.chr(10).chr(13)."<br>";
