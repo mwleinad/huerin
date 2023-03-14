2325f3c68e8d21cb32ca7154e5e83e262ae753c6
aristasoluciones <isc061990@outlook.com>
2023-03-14 01:55:30 -0600

    wewe

--- services/invoice/invoice.php
+++ services/invoice/invoice.php
@@ -481,7 +481,7 @@ class InvoiceService extends Cfdi{
                     "'.$this->currentContract['contractId'].'",
                     "'.$this->currentContract['name'].'",
                     "'.$this->currentContract['rfc'].'",
-                    "'.$_SESSION['errorPac']." ".json_encode($this->data).'"
+                    "'.$_SESSION['errorPac'].' '.json_encode($this->data).'"
                     )';
             $this->Util()->DB()->setQuery($sql);
             $this->Util()->DB()->InsertData();
