00a697d0cc5ed63f7355b2a3492cee7c11acbb4e
Arista Soluciones <isc061990@outlook.com>
2026-01-23 10:33:18 -0600

    Update payment and comprobante schema, fix CFDI date logic
    Added currency type, exchange rate, and original amount columns to the payment table. Modified tipoDeCambio column in comprobante to support higher precision. Updated Cfdi service to use current time for fecha when fechaTimbrado is not set, removing the previous -6 hours offset.

--- services/Cfdi.php
+++ services/Cfdi.php
@@ -187,7 +187,7 @@ class Cfdi extends Comprobante
            $fecha = date('Y-m-d H:i:s', strtotime('-'.$data["fechaTimbrado"].' hours'));
           
         } else {
-            $fecha = date('Y-m-d H:i:s', strtotime('-6 hours'));
+            $fecha = date('Y-m-d H:i:s');
         }
         
         $fechaPago = $this->Util()->FormatDate(time());
